ff51645546e5d91ebcb32f48024b514833a09114
Ádám András <adm.andras@gmail.com>
2021-02-16 11:43:39 +0200

    Upload exercise-3-1 code

---
+++ exercise-3-1/app/src/main/java/eu/neoaren/counter/Counter.java
@@ -0,0 +1,45 @@
+package eu.neoaren.counter;
+
+public class Counter {
+
+    private int counter;
+    private final int max;
+    private final int min;
+    private final int step;
+    private final int initial;
+
+    public Counter(int max, int min, int initial, int step) {
+        this.max = max;
+        this.min = min;
+        this.step = step;
+        this.initial = initial;
+        this.counter = initial;
+    }
+
+    public Counter() {
+        this(100, -100, 0, 1);
+    }
+
+    public void increment() {
+        int newValue = this.counter + this.step;
+        if (this.min <= newValue && newValue <= this.max) {
+            this.counter = newValue;
+        }
+    }
+
+    public void decrement() {
+        int newValue = this.counter - this.step;
+        if (this.min <= newValue && newValue <= this.max) {
+            this.counter = newValue;
+        }
+    }
+
+    public void reset() {
+        this.counter = this.initial;
+    }
+
+    public int getValue() {
+        return this.counter;
+    }
+
+}
---
+++ exercise-3-1/app/src/main/java/eu/neoaren/counter/MainActivity.java
@@ -0,0 +1,72 @@
+package eu.neoaren.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Counter counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up counter
+        this.counter = new Counter(20, -20, 0, 1);
+
+        // Find buttons
+        Button plus = findViewById(R.id.plus);
+        Button minus = findViewById(R.id.minus);
+        ImageButton reset = findViewById(R.id.reset);
+        RadioGroup radioGroup = findViewById(R.id.radioOutput);
+
+        // Attach event listeners
+        plus.setOnClickListener(view -> this.updateCounter(view));
+        minus.setOnClickListener(view -> this.updateCounter(view));
+        reset.setOnClickListener(view -> this.updateCounter(view));
+        radioGroup.setOnCheckedChangeListener((_group, _checkedId) -> this.updateUI());
+
+    }
+
+    // Update counter
+    private void updateCounter(View view) {
+        if (view == findViewById(R.id.plus)) {
+            this.counter.increment();
+        } else if (view == findViewById(R.id.minus)) {
+            this.counter.decrement();
+        } else if (view == findViewById(R.id.reset)) {
+            this.counter.reset();
+        }
+        this.updateUI();
+    }
+
+    // Update user interface
+    @SuppressLint({"SetTextI18n", "NonConstantResourceId"})
+    private void updateUI() {
+        TextView text = findViewById(R.id.counter);
+        RadioGroup rg = findViewById(R.id.radioOutput);
+        switch (rg.getCheckedRadioButtonId()) {
+            case R.id.radioOutputDec: {
+                text.setText(Integer.toString(this.counter.getValue()));
+                break;
+            }
+            case R.id.radioOutputHex: {
+                text.setText(Integer.toHexString(this.counter.getValue()));
+                break;
+            }
+            case R.id.radioOutputBin: {
+                text.setText(Integer.toBinaryString(this.counter.getValue()));
+                break;
+            }
+        }
+    }
+
+}
